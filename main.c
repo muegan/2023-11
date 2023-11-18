26944ec8bebd17e4e70fd0942e03415de5b2b3fd
muegan <2216869@sookmyung.ac.kr>
2023-11-18 18:56:09 +0900

    2번

--- main.c
+++ main.c
@@ -3,24 +3,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+void main(void){
 	
-	int i=10;
-	char c='a';
+	int i=300;
 	
-	int *iptr;
-	iptr=&i;
+	int *pi=&i;
+	char *pc=&i;
 	
-	char *cptr;
-	cptr=&c;
-	
-	int *iptr2;
-	iptr2=iptr;
-	
-	
-	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
-	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
-	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
-	
-	return 0;
+	printf("%i, %i, %i\n", i, *pi, *pc);
 }
