173bea1268d962a86539fdbc57d607079f887589
muegan <2216869@sookmyung.ac.kr>
2023-11-18 19:26:58 +0900

    4번

--- main.c
+++ main.c
@@ -3,16 +3,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap(int *x, int *y){
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
-}
-
-void main(void){
-	int a=3;
-	int b=5;
-	swap(&a,&b);
-	printf("a:%i, b:%i\n", a, b);
+int main(void){
+	
+	int i;
+	int grade[5];
+	
+	for(i=0; i<5; i++){
+		printf("input value (%d) : ",i);
+		scanf("%d", &grade[i]);
+	}
+	
+	
+	int sum=0;
+	int *ptr=grade;
+	
+	for(i=0; i<5; i++){
+		printf("grade[%i] = %i\n", i, *(ptr+i));
+		sum+=grade[i];	
+	}
+	
+	printf("average : %d", sum/5);
 }
